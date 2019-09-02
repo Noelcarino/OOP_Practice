4492c25d261f9d4a8e11ba91fb2fc5b066c896da
Noel Carino <leononirac1992@gmail.com>
2019-09-02 12:08:42 -0700

    added pint changing

--- KOT/monster.js
+++ KOT/monster.js
@@ -2,6 +2,10 @@ class Monster {
     constructor( name, img, initialHitpoints, initialVictoryPoints){
         this.name = name;
         this.image = img;
+        this.maximums = {
+            life: 10,
+            victory: 20
+        }
         this.points = {
             life: initialHitpoints,
             victory: initialVictoryPoints
@@ -12,6 +16,26 @@ class Monster {
             victoryPoints: null
         }
 
+    }
+    changeHitpoints( newValue ) {
+        this.changePoints( 'life', newValue);
+    }
+    changeVictoryPoints( newValue ) {
+        this.changePoints( 'victory', newValue);
+    }
+    changePoints( type, newValue ){
+        var nextValue = this.points[type] + newValue;
+        if (nextValue > this.maximums[type]) {
+            nextValue = this.maximums[type]
+        } else if (nextValue < 0){
+            nextValue = 0;
+        }
+        this.points[type] = nextValue;
+        this.update();
+    }
+    update() {
+        this.domElements.lifePoints.text(this.points.life);
+        this.domElements.victoryPoints.text(this.points.victory);
     }
     render(){
         this.domElements.container = $("<div>", {
